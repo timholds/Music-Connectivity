5be5ec937a7121fef2795dbb5cb0d4fe0c1005ff
timholds <tholdsworth@sandiego.edu>
2018-01-03 19:19:19 -0800

    Genius API get requestadded

--- lyrics-from-genius.py
+++ lyrics-from-genius.py
@@ -1,13 +1,32 @@
-from config import CLIENT_ID, CLIENT_SECRET
+from config import CLIENT_ID, CLIENT_SECRET, CLIENT_ACCESS_TOKEN
+from requests_oauthlib import OAuth2Session
 import requests
 
-def __init__():
+
+def get_lyrics():
+    ''' Connects to the Genius API and gets song lyrics for one or more songs '''
+
+    # Credentials from genius.com
     client_id = CLIENT_ID
     client_secret = CLIENT_SECRET
-    try:
-        song = requests.get(api.geniuscom/songs/378195)
-    except:
-        raise IOError("Couldn't connect to API.")
-    return song
+    token = CLIENT_ACCESS_TOKEN
 
-__init__()
+    # Authorize request when using the API
+    base_url = "http://api.genius.com"
+    headers = {'Authorization': 'Bearer ' + token}
+    search_url = base_url + "/search"
+    song_title = "In the Midst of It All"
+    params = {'q': song_title}
+
+    try:
+        response = requests.get(search_url, params=params, headers=headers)
+        print(response)
+    except Exception as e:
+        response = None
+        print(e)
+    return response
+
+get_lyrics()
+
+
+
